c137934245825005a6fe950034cc5139b67fe4fb
devleandrodias <leandrodbdias@gmail.com>
2020-11-19 13:10:49 -0300

    comecando a trabalhar no compoenente de Todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+
+  constructor() {
+    this.todos.push('ReactJS');
+    this.todos.push('Flutter');
+    this.todos.push('Angular');
+    this.todos.push('Azure');
+    this.todos.push('ASP.Net Core 5.0');
+  }
 }
